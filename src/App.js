feb12febe739f0dfa18d839537fe69fb5fb67c0d
Jorden Ragels <jordenragels3@gmail.com>
2020-04-23 13:46:11 -0600

    card styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list';
 
 class App extends Component {
   constructor() {
@@ -20,6 +21,7 @@ class App extends Component {
 render() {
   return (
     <div className='App'>
+    <CardList />
     {this.state.monsters.map(monster => (
       <h1 key={monster.id}> {monster.name} </h1>
     ))}
